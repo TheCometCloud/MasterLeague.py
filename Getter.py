3e7b5651a9a32b0ca147b07c555bccc4847e3845
TheCometCloud <cometcloud7@gmail.com>
2018-07-28 14:00:30 -0400

    Add find_team and get_record functionality

--- Getter.py
+++ Getter.py
@@ -1,6 +1,9 @@
 import aiohttp
 import time
 import datetime
+import ssl
+from urllib import request
+from bs4 import BeautifulSoup
 from json import loads as jsonify
 
 ROOT_URL = 'https://api.masterleague.net'
@@ -24,24 +27,43 @@ Value: a tuple of type (dict, float)
 
 
 async def fetch(session, url):
+	"""
+	Grabs api text from a url.
+	
+	:param session: aiohttp.ClientSession object
+	:param url: string url of api
+	:return: string response
+	"""
 	async with session.get(url) as response:
 		return await response.text()
 
 
 async def cache_or_load(url):
-		if url not in cache or time.time() - cache[url][1] > 3600:
-			async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
-				string = await fetch(session, url=url)
-				json = jsonify(string)
-				cache[url] = (json, time.time())
-				print(json)
-				return json
-		
-		else:
-			return cache[url][0]
+	"""
+	Saves already found data to the cache and loads data from the api otherwise.
+	:param url: string url of the api
+	:return: string response
+	"""
+	if url not in cache or time.time() - cache[url][1] > 3600:
+		async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+			string = await fetch(session, url=url)
+			json = jsonify(string)
+			cache[url] = (json, time.time())
+			return json
+	
+	else:
+		return cache[url][0]
 
 
 async def search_json(url, field, query=None, get_all=False):
+	"""
+	Searches a json's results field and queries it, returning found matches.
+	:param url: string url of the json
+	:param field: string field name to compare
+	:param query: query to compare with, None by default
+	:param get_all: boolean to get every match, False by default
+	:return: list of json dictionaries that matched, empty if no matches
+	"""
 	results = []
 	json = await cache_or_load(url)
 
@@ -65,11 +87,19 @@ async def search_json(url, field, query=None, get_all=False):
 
 
 async def get_all_heroes():
+	"""
+	Gets a list of all heroes
+	:return: list of hero name strings
+	"""
 	heroes = await search_json(url=ROOT_URL + HEROES_URL, field='name', get_all=True)
 	return heroes
 
 
 async def get_todays_matches():
+	"""
+	Gets all matches occurring today in the blizzard professional scene.
+	:return: list of Match objects
+	"""
 	groups = []
 	matches = []
 	json = await cache_or_load(ROOT_URL + CALENDAR_URL)
@@ -85,41 +115,105 @@ async def get_todays_matches():
 		
 		json = await cache_or_load(json['next'])
 	
-	while True:
-		for i in groups:
-			for j in i['matches']:
-				# TODO: Rework asynchronous instantiation of these classes
-				# More work needs to be done inside the constructor, and less work outside of it.
-				left = await search_json(url=ROOT_URL + TEAMS_URL, field='id', query=j['left_team'])
-				left = left[0]
-				right = await search_json(url=ROOT_URL + TEAMS_URL, field='id', query=j['right_team'])
-				right = right[0]
-				match = Match({'left_team': left, 'right_team': right, 'format': j['format'], 'name': j['name'], 'datetime': j['datetime']})
-				matches.append(match)
+	for i in groups:
+		for j in i['matches']:
+			# TODO: Rework asynchronous instantiation of these classes
+			# More work needs to be done inside the constructor, and less work outside of it.
+			j['left_team'] = (await search_json(url=ROOT_URL + TEAMS_URL, field='id', query=j['left_team']))[0]
+			j['right_team'] = (await search_json(url=ROOT_URL + TEAMS_URL, field='id', query=j['right_team']))[0]
+			match = Match(j)
+			matches.append(match)
 			
-		break
-	
 	return matches
 
 
+async def find_team(name):
+	"""
+	Given a team name, finds its data.
+	:param name: string team name
+	:return: Team object, filled with nones if the team was not found
+	"""
+	team_data = {}
+	try:
+		team_data = (await search_json(url=ROOT_URL + TEAMS_URL, field='name', query=name))[0]
+		
+	except IndexError:
+		pass
+		
+	team = Team(team_data)
+	return team
+
+
 class Team:
+	"""
+	Team object for storing data about particular teams.
+	"""
 	def __init__(self, data):
 		try:
 			team_data = data
 			self.name = team_data['name']
 			self.id = team_data['id']
 			self.logo = team_data['logo']
+			self.region = team_data['region']
 			
-		except IndexError as e:
+		except (IndexError, KeyError):
 			self.name = 'NO NAME'
 			self.id = 'NO ID'
 			self.logo = 'NO LOGO'
+			self.region = 0
 			
+	async def get_record(self):
+		"""
+		Fetches the record of the team in an ongoing tourney.
+		:return: tuple of format (wins, losses) or None if there is no win/loss data
+		"""
+		tourneys = await search_json(url=ROOT_URL + TOURNAMENTS_URL, field='region', query=self.region, get_all=True)
+		event = None
+		for tournament in tourneys:
+			if datetime.datetime.strptime(
+					tournament['start_date'], "%Y-%m-%d").date() \
+					< datetime.date.today() \
+					< datetime.datetime.strptime(tournament['end_date'], "%Y-%m-%d").date():
+				event = tournament
+				break
+				
+		if event:
+			standing = None
+			url = ''
+			for i in event['stages']:
+				if i['name'] == 'Standings':
+					url = f'https://masterleague.net/tournament/stage/{i["id"]}/'
+			
+			if url:
+				context = ssl._create_unverified_context()
+				source = request.urlopen(url, context=context).read()
+				soup = BeautifulSoup(source, 'lxml')
+				
+				table = soup.find('table', attrs={'class': 'table table-hover table-condensed table-fixed standings'})
+				table_rows = table.find_all('tr')
+				
+				data = []
+				for tr in table_rows:
+					td = tr.find_all('td')
+					row = [tr.text for tr in td]
+					data.append(row)
+					
+				for line in data:
+					if len(line) >= 3:
+						if line[0] == self.name:
+							return line[1], line[2]
+						
+		return None
+
 
 class Match:
+	"""
+	Match object for storing data about a particular match.
+	"""
 	def __init__(self, data):
 		self.left_team = Team(data['left_team'])
 		self.right_team = Team(data['right_team'])
 		self.format = data['format']
 		self.when = data['datetime']
 		self.name = data['name']
+	
--- Tests.py
+++ Tests.py
@@ -7,10 +7,37 @@ class GetterTestCase(unittest.TestCase):
 	
 	def setUp(self):
 		self.loop = asyncio.get_event_loop()
+		
+	def tearDown(self):
+		del self.loop
 	
 	def test_get_heroes(self):
 		heroes = self.loop.run_until_complete(Getter.get_all_heroes())
 		self.assertTrue('Abathur' in heroes)
+		
+	def test_get_games(self):
+		matches = self.loop.run_until_complete(Getter.get_todays_matches())
+		self.assertTrue(len(matches) > 0)
+		
+	def test_get_record(self):
+		team = Getter.Team({
+			'id': 29,
+			'region': 2,
+			'logo': 'https://c.masterleague.net/media/team/logo/1515844114.jpg.300x300_q85.jpg',
+			'name': 'Tempo Storm'
+		})
+		self.loop.run_until_complete(team.get_record())
+		self.assertTrue(team)
+		
+	def test_find_team_by_name(self):
+		name = 'Tempo Storm'
+		team = self.loop.run_until_complete(Getter.find_team(name))
+		self.assertTrue(team.name == name)
+		
+	def test_returns_none(self):
+		name = 'Theres no way that this is a team name.'
+		team = self.loop.run_until_complete(Getter.find_team(name))
+		self.assertTrue(team.name == 'NO NAME')
 		
 		
 if __name__ == '__main__':
